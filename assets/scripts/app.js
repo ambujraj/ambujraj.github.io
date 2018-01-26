3ee8315611396bdfd7aaa07e06f2d21ff207e631
Ambuj <thecoderguy@outlook.com>
2018-01-26 12:03:19 +0530

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,81 @@
+const Portfolio = function() {
+	function makeWords() {
+		var words = [
+			{
+				text: "css3",
+				weight: 8
+			}, {
+				text: "python",
+				weight: 12
+			}, {
+				text: "jquery",
+				weight: 3
+			}, {
+				text: "programming",
+				weight: 7
+			}, {
+				text: "javascript",
+				weight: 10
+			}, {
+				text: "code",
+				weight: 9
+			}, {
+				text: "Ambuj",
+				weight: 15
+			}, {
+				text: "newText",
+				weight: 7
+			}
+		];
+		return words;
+	}
+
+	function makeWordCloud(words) {
+		$('.teaching-domains').jQCloud(words, {delay: 120});
+	}
+
+	function displayWordCloud() {
+		var count = 1;
+		$(window).on('scroll', function() {
+			var y_scroll_pos = window.pageYOffset;
+			var scroll_pos_test = 2700; // set to whatever you want it to be
+			var words = makeWords();
+			if (y_scroll_pos > scroll_pos_test && count <= 1) {
+				makeWordCloud(words);
+				count++;
+			}
+		});
+	}
+
+	function designForm() {
+		$("#my-modal form").addClass("my-form");
+	}
+
+	function typeAnimation() {
+		Typed.new("#writing-text", {
+			strings: [
+				"am a Python Programmer.","am a Music lover.","am also a full-stack Developer","am making my hands dirty on Machine Learning", "solve problems.", "love everything about code."
+			],
+			
+			stringsElement: null,
+			
+			typeSpeed: 1,
+			contentType: 'text',
+			callback: function() {
+				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
+			},
+			preStringTyped: function() {},
+			onStringTyped: function() {}
+		});
+	}
+
+	return {
+		displayWordCloud: displayWordCloud,
+		typeAnimation: typeAnimation
+	}
+
+}();
+
+
+Portfolio.displayWordCloud();
+Portfolio.typeAnimation();
